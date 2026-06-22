a3fc4ff4d75444bad8a42858a3350a00671a7a26
Benjamin <219252301+specialbean@users.noreply.github.com>
2026-06-22 23:08:45 +0200

    slightly extended reader class, nightly save

--- src/includes/messages.hpp
+++ src/includes/messages.hpp
@@ -2,11 +2,12 @@
 
 #include <cstdint>
 
-
-
 #pragma pack(push, 1)
-
 
 struct systemEventMessage {
     char message_type = 'S';
+    uint16_t stock_locate = 0;
+    uint16_t tracking_number;
+    uint64_t timestamp_nanoseconds;
+    char event_code;
 };
--- src/reader.cpp
+++ src/reader.cpp
@@ -1,11 +1,29 @@
 #pragma once
+#include "common/utils.h"
 #include <cstdint>
+#include <fstream>
 #include <iostream>
 #include <stdio.h>
-#include <fstream>
-
+#include <vector>
 
 class Reader {
+    std::ifstream file;
 
+    char *returnHexMessage(int length) {
+        char buf[length];
+        file.read(buf, length);
+        return;
+    }
 
+  public:
+    Reader(std::string file_name) {
+        file.setf(std::ios_base::hex);
+        file.open(file_name);
+    }
+
+    int getNextHexMessage() {
+        char buf[2];
+        file.read(buf, 2);
+        hexPairToByte(buf[0], buf[1]);
+    }
 };
