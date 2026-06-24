0ba865532314d24b18e51165170f267ad0ff09f0
Benjamin <219252301+specialbean@users.noreply.github.com>
2026-06-24 22:06:30 +0200

    added stockDirectoryMessage struct and handler

--- src/includes/messages.hpp
+++ src/includes/messages.hpp
@@ -12,4 +12,25 @@ struct systemEventMessage {
     char event_code;
 };
 
+struct stockDirectoryMessage {
+    char message_type = 'R';
+    uint16_t stock_locate = 0;
+    uint16_t tracking_number;
+    uint64_t timestamp_nanoseconds;
+    char stock[8];
+    char market_category;
+    char financial_status_indicator;
+    uint32_t round_lot_size;
+    char round_lots_only;
+    char issue_classification;
+    char issue_sub_type[2];
+    char authenticity;
+    char short_sale_threshold_indicator;
+    char ipo_flag;
+    char luld_reference_price_tier;
+    char etp_flag;
+    char etp_leverage_factor;
+    char inverse_indicator;
+};
+
 #pragma pack(pop)
--- src/main.cpp
+++ src/main.cpp
@@ -4,34 +4,64 @@
 #include <iostream>
 #include <vector>
 
-namespace nasdaq {
-
-namespace tooling {
-template <typename T>
-
-auto msgToFormat(const char *buf, size_t &offset) -> T {
+template <typename T> auto msgToFormat(const char *buf, size_t &offset) -> T {
     T res;
     std::memcpy(&res, buf + offset, sizeof(T));
     offset += sizeof(T);
     return res;
 }
-} // namespace tooling
 
-auto parseMessage(systemEventMessage &msg, const char *buf, size_t &offset)
-    -> void {
-    msg.message_type = nasdaq::tooling::msgToFormat<char>(buf, offset);
+auto msgStringToFormat(const char *buf, size_t &offset, char *res,
+                       uint16_t string_length) -> void {
+    std::memcpy(res, buf + offset, string_length);
 }
-} // namespace nasdaq
+
+auto formatMessage(systemEventMessage &msg, const char *buf, size_t &offset)
+    -> void {
+    msg.stock_locate = msgToFormat<uint16_t>(buf, offset);
+    msg.tracking_number = msgToFormat<uint16_t>(buf, offset);
+    msg.timestamp_nanoseconds = msgToFormat<uint64_t>(buf, offset);
+    msg.event_code = msgToFormat<char>(buf, offset);
+}
+
+auto formatMessage(stockDirectoryMessage &msg, const char *buf, size_t &offset)
+    -> void {
+    msg.stock_locate = msgToFormat<uint16_t>(buf, offset);
+    msg.tracking_number = msgToFormat<uint16_t>(buf, offset);
+    msg.timestamp_nanoseconds = msgToFormat<uint64_t>(buf, offset);
+
+    msgStringToFormat(buf, offset, msg.stock, 8);
+    msg.market_category = msgToFormat<char>(buf, offset);
+    msg.financial_status_indicator = msgToFormat<char>(buf, offset);
+    msg.round_lot_size = msgToFormat<uint32_t>(buf, offset);
+    msg.round_lots_only = msgToFormat<char>(buf, offset);
+    msg.issue_classification = msgToFormat<char>(buf, offset);
+
+    msgStringToFormat(buf, offset, msg.issue_sub_type, 2);
+    msg.authenticity = msgToFormat<char>(buf, offset);
+    msg.short_sale_threshold_indicator = msgToFormat<char>(buf, offset);
+    msg.ipo_flag = msgToFormat<char>(buf, offset);
+    msg.luld_reference_price_tier = msgToFormat<char>(buf, offset);
+    msg.etp_flag = msgToFormat<char>(buf, offset);
+    msg.etp_leverage_factor = msgToFormat<uint32_t>(buf, offset);
+    msg.inverse_indicator = msgToFormat<char>(buf, offset);
+}
+
+/* impelement function to determine message_type
+auto parse(std::vector<char> *buf) -> void {
+
+}
+*/
 
 int main() {
-    Reader *x = new Reader("../tests/testing_systeMessage.itch");
+    Reader *x = new Reader("../tests/t_SystemEventMessage.itch");
 
     auto buf{x->bufferMessage()};
 
     systemEventMessage temp{};
     std::size_t off{sizeof(uint16_t)};
 
-    nasdaq::parseMessage(temp, buf.data(), off);
+    formatMessage(temp, buf.data(), off);
 
     std::cout << temp.message_type << '\n';
 }
